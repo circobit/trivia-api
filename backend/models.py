e2dfdd9eab3e4a6e3bb1b61b795fe6d73403a2b9
Cristian Cevasco <cristian.cevasco@gmail.com>
2025-10-03 15:19:15 +0200

    feat(models): add insert, update and delete methods for Category

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 database_name = 'trivia'
-database_user = 'postgres'
-database_password = 'password'
+database_user = 'cristiancevasco'
+database_password = ''
 database_host = 'localhost:5432'
 database_path = f'postgresql://{database_user}:{database_password}@{database_host}/{database_name}'
 
@@ -66,6 +66,17 @@ class Category(db.Model):
 
     def __init__(self, type):
         self.type = type
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def format(self):
         return {
